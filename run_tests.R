b2be2ba0b1afdbdd5346a6c73f77daf00516c3b8
Don Boyd <donboyd5@gmail.com>
2024-08-12 12:43:04 -0400

    WIP

--- run_tests.R
+++ run_tests.R
@@ -7,14 +7,17 @@ library(testthat)
 test_dir("tests/testthat")
 
 
-# Run tests with Summary reporter
-# test_dir("tests/testthat", reporter = ProgressReporter)
-# test_results <- test_file("tests/testthat/test_compare_baseline.R")
-# print(test_results)
-# 
-# test_results <- test_file("tests/testthat/test_compare_salary_headcount.R")
-# print(test_results)
+# how to create a test runner
 
-# run this with source("run_tests.R") in the console
-
+# run_tests <- function() {
 #   # Load setup once
+#   source("path/to/your/setup/script.R")
+#   
+#   # Run tests
+#   test_file("tests/test_file1.R")
+#   test_file("tests/test_file2.R")
+#   # Add more test files as needed
+# }
+# 
+# # Run the tests
+# run_tests()
