49b1a1ad365ad2d01bbe1780f3b3276058c4503f
Don Boyd <donboyd5@gmail.com>
2024-08-12 14:13:10 -0400

    make test-running more flexible

--- run_tests.R
+++ run_tests.R
@@ -2,22 +2,31 @@
 
 library(testthat)
 
+run_tests <- function() {
+  # Load setup once
+  source("tests/testthat/initial_setup.R")
+
+  # Run tests
+  test_file("tests/testthat/test_compare_baseline.R")
+  test_file("tests/testthat/test_compare_salary_headcount.R")
+  # Add more test files as needed
+}
+
+# Run the tests
+run_tests()
+
+
+# Cleanup code
+# Remove objects or environments created during setup
+rm(list = ls(envir = oldws), envir = oldws)
+rm(oldws)
+
+rm(list = ls(envir = newws), envir = newws)
+rm(newws)
+
+
+
+
 # test_file("tests/testthat/test_compare_baseline.R")
 # test_file("tests/testthat/test_compare_salary_headcount.R")
-test_dir("tests/testthat")
-
-
-# how to create a test runner
-
-# run_tests <- function() {
-#   # Load setup once
-#   source("path/to/your/setup/script.R")
-#   
-#   # Run tests
-#   test_file("tests/test_file1.R")
-#   test_file("tests/test_file2.R")
-#   # Add more test files as needed
-# }
-# 
-# # Run the tests
-# run_tests()
+# test_dir("tests/testthat")
