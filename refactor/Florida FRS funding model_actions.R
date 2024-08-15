6c92b9d518e01c8900481d96f87dfc58c107914c
Don Boyd <donboyd5@gmail.com>
2024-08-15 15:28:38 -0400

    create function to get current_amort_layers_table

--- refactor/Florida FRS funding model_actions.R
+++ refactor/Florida FRS funding model_actions.R
@@ -6,7 +6,19 @@ get_all_classes_funding_list <- function(class_names){
   return(funding_list)
 }
 
-
+get_current_amort_layers_summary_table <- function(current_amort_layers_table_){
+  #Summarize current amortization layers
+  current_amort_layers_table <- current_amort_layers_table_ %>% 
+    mutate(amo_period = if_else(amo_period == "n/a", "20", amo_period),
+           amo_period = as.numeric(amo_period)) %>% 
+    group_by(class, amo_period) %>%
+    summarise(amo_balance = sum(amo_balance)) %>% 
+    #make sure that the amo periods are arranged in descending order
+    arrange(class, desc(amo_period)) %>% 
+    ungroup()
+  
+  return(current_amort_layers_table)
+}
 
 
 # groups for class names:
@@ -15,17 +27,12 @@ class_names_no_drop_frs <- class_names[!class_names %in% c("drop", "frs")]
 class_names_no_frs <- class_names[!class_names %in% c("frs")]
 
 funding_list <- get_all_classes_funding_list(class_names)
+current_amort_layers_table <- get_current_amort_layers_summary_table(current_amort_layers_table_)
 
 
-#Summarize current amortization layers
-current_amort_layers_table <- current_amort_layers_table_ %>% 
-  mutate(amo_period = if_else(amo_period == "n/a", "20", amo_period),
-         amo_period = as.numeric(amo_period)) %>% 
-  group_by(class, amo_period) %>%
-  summarise(amo_balance = sum(amo_balance)) %>% 
-  #make sure that the amo periods are arranged in descending order
-  arrange(class, desc(amo_period)) %>% 
-  ungroup()
+
+
+
 
 
 
