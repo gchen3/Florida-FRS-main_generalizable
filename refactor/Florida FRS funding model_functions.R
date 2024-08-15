5950f6b14f9141283c5424faafa010440b2f2c6d
Don Boyd <donboyd5@gmail.com>
2024-08-15 15:45:31 -0400

    Move new functions to "Florida FRS funding model_functions.R"

--- refactor/Florida FRS funding model_functions.R
+++ refactor/Florida FRS funding model_functions.R
@@ -3,7 +3,36 @@
 #################################################################
 
 
-####Data preparation
+# functions for funding list and amortization layers ----------------------
+
+# these functions are NOT needed in the get_funding_data function, but are called before it is called
+
+
+get_all_classes_funding_list <- function(class_names){
+  funding_list <- lapply(class_names, get_funding_table)
+  names(funding_list) <- class_names
+  
+  return(funding_list)
+}
+
+get_current_amort_layers_summary_table <- function(current_amort_layers_table_){
+  #Summarize current amortization layers
+  current_amort_layers_table <- current_amort_layers_table_ %>% 
+    mutate(amo_period = if_else(amo_period == "n/a", "20", amo_period),
+           amo_period = as.numeric(amo_period)) %>% 
+    group_by(class, amo_period) %>%
+    summarise(amo_balance = sum(amo_balance)) %>% 
+    #make sure that the amo periods are arranged in descending order
+    arrange(class, desc(amo_period)) %>% 
+    ungroup()
+  
+  return(current_amort_layers_table)
+}
+
+
+
+
+#### Data preparation
 #Create 9 empty data frames from the init_funding_data (representing 7 classes, DROP, and FRS system), then put them in a list to store funding outputs for these entities
 get_funding_table <- function(class_name) {
   funding_table <- init_funding_data %>% 
