b22aa4f5a174f1d6f84e8dd7617f1f1c11ac69d0
Don Boyd <donboyd5@gmail.com>
2024-08-27 12:55:43 -0400

    improve tests

--- refactor/tools/run_new_baseline_and_save_new_results.R
+++ refactor/tools/run_new_baseline_and_save_new_results.R
@@ -10,28 +10,27 @@ rm(list = ls())
 system.time(source(here::here("refactor", "R", "FRS_master.R"))) # load and build the full FRS Florida model
 
 # generate baseline results
-system.time(baseline_funding <- get_funding_data(funding_list = funding_list, 
-                                                 current_amort_layers_table = current_amort_layers_table, 
-                                                 wf_data_list = wf_data_list,
-                                                 entrant_profile_table_list = entrant_profile_table_list, 
-                                                 salary_headcount_table_list = salary_headcount_table_list, 
-                                                 mort_table_list = mort_table_list, 
-                                                 mort_retire_table_list = mort_retire_table_list, 
-                                                 separation_rate_table_list = separation_rate_table_list,
+system.time(baseline_funding <- get_funding_data(funding_list = params$funding_list, 
+                                                 current_amort_layers_table = params$current_amort_layers_table, 
+                                                 wf_data_list = params$wf_data_list,
+                                                 entrant_profile_table_list = params$entrant_profile_table_list, 
+                                                 salary_headcount_table_list = params$salary_headcount_table_list, 
+                                                 mort_table_list = params$mort_table_list, 
+                                                 mort_retire_table_list = params$mort_retire_table_list, 
+                                                 separation_rate_table_list = params$separation_rate_table_list,
                                                  params=params)) # about 60 secs tot, ~ 10 seconds for a single class
 
 system.time(baseline_liability <- get_liability_data(class = "regular", 
-                                                     wf_data = regular_wf_data, 
+                                                     wf_data = params$wf_data_list$regular_wf_data, 
                                                      ben_payment_current = params$regular_ben_payment_current_,
                                                      retiree_pop_current = params$regular_retiree_pop_current_,
                                                      pvfb_term_current = params$regular_pvfb_term_current_,
-                                                     entrant_profile_table = regular_entrant_profile_table,
-                                                     salary_headcount_table = regular_salary_headcount_table,
-                                                     mort_table = regular_mort_table,
-                                                     mort_retire_table = regular_mort_retire_table,
-                                                     separation_rate_table = regular_separation_rate_table,
-                                                     params = params)
-            ) # ~ 10 seconds for a single class
+                                                     entrant_profile_table = params$entrant_profile_table_list$regular_entrant_profile_table,
+                                                     salary_headcount_table = params$salary_headcount_table_list$regular_salary_headcount_table,
+                                                     mort_table = params$mort_table_list$regular_mort_table,
+                                                     mort_retire_table = params$mort_retire_table_list$regular_mort_retire_table,
+                                                     separation_rate_table = params$separation_rate_table_list$regular_separation_rate_table,
+                                                     params = params)) # ~ 10 seconds for a single class
 
 system.time(save.image(here::here("refactor", "new_results", "new_workspace.RData"))) # save the entire workspace, ~ 15-20 secs
 
