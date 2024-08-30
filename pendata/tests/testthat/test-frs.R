4e33146655ccc6718426888c10dc46220cf2f712
Don Boyd <donboyd5@gmail.com>
2024-08-30 08:50:23 -0400

    further modifications so that packge will pass check

--- pendata/tests/testthat/test-frs.R
+++ pendata/tests/testthat/test-frs.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+library(tidyverse)
+
+reformat_base_mortality <- function(etype) {
+  dfnew <- frs$base_mort_table |>
+    filter(employee_type == etype) |>
+    pivot_wider(names_from = c(beneficiary_type, gender), values_from = rate) |>
+    select(all_of(names(dfold)))
+
+  # ensure compatibility with Reason object classes
+  if(etype == "regular") dfnew <- as.data.frame(dfnew)
+
+  return(dfnew)
+}
+
+reason <- readRDS(test_path("testdata", "reason.rds"))
+
+for (etype in c("general", "regular", "teacher")) {
+  print(etype)
+  dfold <- reason[[paste0("base_", etype, "_mort_table")]]
+  dfnew <- reformat_base_mortality(etype)
+  expect_equal(dfnew, dfold)
+}
+
