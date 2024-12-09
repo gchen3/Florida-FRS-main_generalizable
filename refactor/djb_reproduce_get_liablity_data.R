ea82739fbdf5398abe85d8d10e08aee5b3d9e44f
Don Boyd <donboyd5@gmail.com>
2024-12-09 07:26:11 -0500

    complete benefit_val_table_stacked, faster

--- refactor/djb_reproduce_get_liablity_data.R
+++ refactor/djb_reproduce_get_liablity_data.R
@@ -85,16 +85,6 @@ salary_headcount_table_stacked <- stack_list(params$salary_headcount_table_list,
 count(salary_headcount_table_stacked, class)
 params$salary_headcount_table_list$admin_salary_headcount_table # 26 admin rows
 
-thelist <- params$salary_headcount_table_list
-
-stack_list <- function(thelist, suffix) {
-  tmp <- bind_rows(thelist, .id = "name") |> 
-    mutate(class = str_remove(name, suffix)) |>
-    select(-name) |> 
-    select(class, everything())
-}
-
-
 #.. mort_table ----
 # ns(params$mort_table_list) |> str_subset("_mort_table")
 mort_table_stacked <- stack_list(params$mort_table_list, "_mort_table")
@@ -329,7 +319,7 @@ ann_factor_table_stacked <- as_tibble(ann_factor_table_stacked)
 bb <- proc.time()
 bb - aa
 
-
+# for checking ----
 ann_factor_table <- mort_table %>% 
   #Semi join the salary_benefit_able to reduce the size of the data that needs to be calculated
   semi_join(salary_benefit_table, by = c("entry_year", "entry_age")) %>%
@@ -629,7 +619,7 @@ get_dist_age_table_stacked <- function(benefit_table_stacked){
 }
 
 system.time(dist_age_table_stacked <- get_dist_age_table_stacked(benefit_table_stacked)) # 2.3 secs
-dist_age_table <- bm_env$get_dist_age_table(benefit_table)
+# dist_age_table <- bm_env$get_dist_age_table(benefit_table) # for checking
 
 # END dist_age_table_stacked ----
 
@@ -653,7 +643,7 @@ get_final_benefit_table_stacked <- function(benefit_table_stacked, dist_age_tabl
 }
 
 system.time(final_benefit_table_stacked <- get_final_benefit_table_stacked(benefit_table_stacked, dist_age_table_stacked))
-final_benefit_table <- bm_env$get_final_benefit_table(benefit_table, dist_age_table)
+# final_benefit_table <- bm_env$get_final_benefit_table(benefit_table, dist_age_table)  # checks
 
 # END final_benefit_table_stacked ----
 
@@ -670,8 +660,58 @@ tierterm_lookup <- salary_benefit_table_stacked |>
   distinct() |> 
   as_tibble() |> 
   mutate(tier = str_sub(tier_at_term_age, 6, 6),
-         term_status=str_sub(tier_at_term_age, 8, -1))
+         term_status=str_sub(tier_at_term_age, 8, -1),
+         sep_type = case_when(
+           str_detect(tier_at_term_age, "early|norm|reduced") ~ "retire",
+           str_detect(tier_at_term_age, "non_vested") ~ "non_vested",
+           str_detect(tier_at_term_age, "vested") ~ "vested",
+           .default = NULL # Reason did not give an explicit default, which means it is NULL; perhaps NA_character_ would be better
+           ))
 tierterm_lookup
+
+library(data.table)
+
+npv <- function(cashflows, rate, immediate=FALSE) {
+  n <- length(cashflows)
+  if (immediate) { # cash flows at beginning of period
+    powers <- 0:(n-1)
+  } else {  # cash flows at end of period
+    powers <- 1:n
+  }
+  sum(cashflows / (1 + rate)^powers)
+}
+
+get_pvfb <- function(sep_rate_vec, interest_vec, value_vec){ 
+  pv <- function(i){
+    sep_prob <- cumprod(1 - data.table::shift(sep_rate_vec[i:end], n = 2, fill = 0)) * 
+      data.table::shift(sep_rate_vec[i:end], n = 1, fill = 0)
+    cashflow <- sep_prob * value_vec[i:end]
+    npv(cashflow, interest_vec[i], immediate = TRUE)
+  }
+  end <- length(sep_rate_vec)
+  # loop below has proven to be faster than purrr::map()
+  pvfb <- double(length = end)
+  for (i in 1:end) {
+    pvfb[i] <- pv(i)
+  }
+  # CAUTION: temporary treatment to replace zeros with NAs to match FRS model results
+  pvfb[pvfb == 0] <- NA
+  pvfb
+}
+
+get_pvfs <- function(remaining_prob_vec, interest_vec, sal_vec){ 
+  pv <- function(i){
+    saladj <- sal_vec[i:end] * remaining_prob_vec[i:end] /  remaining_prob_vec[i]
+    npv(saladj, interest_vec[i], immediate = FALSE)
+  }
+  end <- length(sal_vec)
+  pvfs <- double(length = end)
+  for (i in 1:end) {
+    pvfs[i] <- pv(i)
+  }
+  pvfs
+}
+
 
 get_benefit_val_table_stacked <- function(
     salary_benefit_table_stacked,
@@ -684,41 +724,56 @@ get_benefit_val_table_stacked <- function(
               by = join_by(class, entry_age, entry_year, term_age)) |> 
     left_join(separation_rate_table_stacked,
               by = join_by(class, entry_year, entry_age, yos, term_age))  |> 
-    left_join(tierterm_lookup,
+    left_join(tierterm_lookup |> 
+                select(tier_at_term_age, tier, sep_type),
               by = join_by(tier_at_term_age)) |>
     mutate(
       # note that the tier below applies at termination age only
       dr = if_else(tier=="3", params$dr_new_, params$dr_current_),
-      sep_type = get_sep_type(tier_at_term_age),
-      ben_decision = if_else(yos == 0, 
-                             NA, 
-                             if_else(sep_type == "retire", "retire",
-                                     if_else(sep_type == "vested", "mix", "refund"))),
+      ben_decision = case_when(
+        yos == 0 ~ NA_character_,
+        sep_type == "retire" ~ "retire",
+        sep_type == "vested" ~ "mix",
+        sep_type == "non_vested" ~ "refund",
+        .default = NA_character_
+      ),
       pvfb_db_wealth_at_term_age = case_when(
         sep_type == "retire" ~ pvfb_db_at_term_age,
-        sep_type == "vested" ~ (params$retire_refund_ratio_ * pvfb_db_at_term_age + (1 - params$retire_refund_ratio_) * db_ee_balance),
+        sep_type == "vested" ~ (params$retire_refund_ratio_ * pvfb_db_at_term_age + 
+                                  (1 - params$retire_refund_ratio_) * db_ee_balance),
         sep_type == "non_vested" ~ db_ee_balance
-      )
-    ) %>% 
-    group_by(entry_year, entry_age) %>%
+      ))|> 
     mutate(
-      #calculate the present value of future DB benefits at current age (discount the annual DB benefits back to current age)
-      pvfb_db_wealth_at_current_age = get_pvfb(sep_rate_vec = separation_rate, interest_vec = dr, value_vec = pvfb_db_wealth_at_term_age),
+      # instead of pentools::get_pvfb
+      pvfb_db_wealth_at_current_age = get_pvfb(sep_rate_vec = separation_rate,
+                                                         interest_vec = dr,
+                                                         value_vec = pvfb_db_wealth_at_term_age),
       
-      #calculate the present value of future salary at current age (discount the annual salary back to current age)
-      pvfs_at_current_age = get_pvfs(remaining_prob_vec = remaining_prob, interest_vec = dr, sal_vec = salary),
+      # calculate the present value of future salary at current age (discount the annual salary back to current age)
+      # instead of pentools::get_pvfs
+      pvfs_at_current_age = get_pvfs(remaining_prob_vec = remaining_prob,
+                                               interest_vec = dr,
+                                               sal_vec = salary),
       
-      #calculate the individual normal cost rate at current age
+      # calculate the individual normal cost rate at current age
       indv_norm_cost = pvfb_db_wealth_at_current_age[yos == 0] / pvfs_at_current_age[yos == 0],
       
-      #calculate the present value of future normal cost at current age (discount the annual normal cost back to current age)
-      pvfnc_db = indv_norm_cost * pvfs_at_current_age
-    ) %>% 
-    ungroup()
+      # calculate the present value of future normal cost at current age (discount the annual normal cost back to current age)
+      pvfnc_db = indv_norm_cost * pvfs_at_current_age,
+      .by = c(class, entry_year, entry_age)
+    )
   
   return(benefit_val_table_stacked)
 }
 
+a <- proc.time()
+benefit_val_table_stacked <- get_benefit_val_table_stacked(
+  salary_benefit_table_stacked,
+  final_benefit_table_stacked,
+  separation_rate_table_stacked,
+  params)
+b <- proc.time()
+b - a
 
 # END benefit_val_table_stacked ----
 
