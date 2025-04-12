704af05b8124261e2ea9d3faac352cbe9e9a85d6
Don Boyd <donboyd5@gmail.com>
2025-04-12 06:30:14 -0400

    show stacked tables for pendata info

---
+++ djb_show_stacked_tables.R
@@ -0,0 +1,11 @@
+
+fde <- frs_data_env
+fde$salary_headcount_table
+fde$term_rate_age
+fde$mort_table
+fde$entrant_profile_table
+fde$mort_retire_table
+fde$normal_retire_rate_table
+fde$early_retire_rate_table
+fde$separation_rate_table
+fde$salary_growth_table
